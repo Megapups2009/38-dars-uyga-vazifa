fdd1e017ffcf0af829eea4f553d79c0d6a978ea5
Megapups2009 <122957393+Megapups2009@users.noreply.github.com>
2023-01-29 20:42:54 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,81 @@
+// let n = 2;
+// let result = n *= 5
+
+
+// alert(result)
+// let num = 1;
+
+// increment
+
+// alert (num++);
+// alert(num++);
+// alert (num++);
+// alert (num++);
+// alert (num++);
+// alert (num++);
+// alert (num++);
+// alert (num++);
+// alert (num++);
+// alert (num++);
+// alert (num++);
+
+// let num = 10; 
+
+// discrement
+// alert(num--)
+// alert(num--)
+// alert(num--)
+// alert(num--)
+// alert(num--)
+// alert(num--)
+// alert(num--)
+// alert(num--)
+// alert(num--)
+// alert(num--)
+// alert(num--)
+
+// let i = 0 
+// do{
+//     alert(i);
+//     i++;
+// }while (i < 3);
+// let son = 1;
+// while(son <= 10) {
+//     alert(son)
+//     son++
+// }
+// let num = 10;
+// while(num >= 1) {
+//     alert(num)
+//     num--
+
+// }
+
+// for( let i = 1; i <= 10; i++) {
+//     alert(i)
+// }
+
+// for(let i = 1; i <= 10; i++){
+//     if(i % 2 == 0) continue;
+//     alert(i);
+// }
+
+// for(let i = 1; i < 10; i++) {
+//     if(i % 2 == 1)  
+//     document.write(i); 
+// }
+// for(let i = 2; i <= 9; i++) {
+//     for(let j = 1; j <= 10; j++) {
+//         document.write(`${i}  * ${j} = ${i*j} <br>`);
+//     }
+// }
+
+
+ let lines = 8;
+ let str = " ";
+ let star = "*";
+ for (let i = 0; i < lines; i++) {
+   str += star;
+   console.log(str);
+ }
+
